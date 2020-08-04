6c975442e1e8fce7c30835d3ec2e73ed1b4c783b
garimagupta3 <46891596+garimagupta3@users.noreply.github.com>
2020-08-04 13:34:37 +0530

    Update ExfriendManager.java

--- app/src/main/java/nil/nadph/qnotified/ExfriendManager.java
+++ app/src/main/java/nil/nadph/qnotified/ExfriendManager.java
@@ -57,6 +57,7 @@ import static nil.nadph.qnotified.util.ActProxyMgr.ACTION_EXFRIEND_LIST;
 import static nil.nadph.qnotified.util.ActProxyMgr.ACTIVITY_PROXY_ACTION;
 import static nil.nadph.qnotified.util.Initiator.load;
 import static nil.nadph.qnotified.util.Utils.*;
+import static nil.nadph.qnotified.util.Utils.*;
 
 public class ExfriendManager implements SyncUtils.OnFileChangedListener {
     static public final int ID_EX_NOTIFY = 65537;
